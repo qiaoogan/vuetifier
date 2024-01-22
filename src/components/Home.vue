3edf99e1b391e62b9bbddc94b1a7555df5867048
qiaoogan <qiaogang1984@163.com>
2024-01-22 17:47:57 +0800

    add router

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,5 +1,36 @@
 <template>
-  <div>Why?????????????????</div>
+  <v-card>
+    <v-layout>
+      <v-navigation-drawer
+        expand-on-hover
+        rail
+      >
+        <v-list>
+          <v-list-item
+            prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+            title="Sandra Adams"
+            subtitle="sandra_a88@gmailcom"
+          ></v-list-item>
+        </v-list>
+
+        <v-divider></v-divider>
+
+        <v-list density="compact" nav>
+
+            <v-list-item prepend-icon="mdi-folder" title="Profile" value="profile" to="/home/profile"></v-list-item>
+            <v-list-item prepend-icon="mdi-account-multiple" title="Update" value="update" to="/home/update"></v-list-item>
+            <v-list-item prepend-icon="mdi-star" title="About" value="about" to="/home/about"></v-list-item>
+
+        </v-list>
+      </v-navigation-drawer>
+
+      <v-main style="height: 250px">
+        <router-view></router-view>
+      </v-main>
+
+    </v-layout>
+  </v-card>
+
 </template>
 
 <script setup>
--- src/routers/index.js
+++ src/routers/index.js
@@ -2,10 +2,29 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 import HelloWorld from '@/components/HelloWorld.vue'
 import Home from '@/components/Home.vue'
+import Profile from '@/components/Profile.vue'
+import Update from '@/components/Update.vue'
+import About from '@/components/About.vue'
 
 const routes = [
   { path: '/', component: HelloWorld },
-  { path: '/home', component: Home }
+  { path: '/home',
+    component: Home,
+    children: [
+      {
+        path: 'profile',
+        component: Profile,
+      },
+      {
+        path: 'update',
+        component: Update,
+      },
+      {
+        path: 'about',
+        component: About,
+      },
+    ]
+  }
 ]
 
 export const router = createRouter({
